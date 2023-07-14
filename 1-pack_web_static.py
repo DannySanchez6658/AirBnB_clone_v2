5232222d2c5496803add917fb14f426f68a066af
DannySanchez6658 <danieltsega6658@gmail.com>
2023-07-14 07:45:29 +0000

    [Fix]: Fixed the code

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,31 +1,19 @@
 #!/usr/bin/python3
-'''
-Module: '1-pack_web_static'
-Script that generates a .tgz archive from contents of web_static folder
-'''
-from datetime import datetime
-from fabric.api import local
+from fabric.api import *
 import os
+from datetime import datetime
+
+env.hosts = ['localhost']
 
 
 def do_pack():
-    """generates a tgz archive"""
-    if not os.path.isdir("versions"):
-        os.mkdir("versions")
-    date = datetime.now()
-    file_name = "versions/web_static_{}{}{}{}{}{}.tgz".format(
-            date.year,
-            date.month,
-            date.day,
-            date.hour,
-            date.minute,
-            date.second
-            )
     try:
-        print("Packing web_static to {}".format(file_name))
-        local("tar -cvzf {} web_static".format(file_name))
-        arch_size = os.stat(file_name).st_size
-        print("web_static packed: {} -> {} Bytes".format(file_name, arch_size))
-    except Exception:
-        return None
-    return file_name
+        filepath = "versions/web_static_" + datetime.now().\
+                   strftime("%Y%m%d%H%M%S") + ".tgz"
+        local("mkdir -p versions")
+        local("tar -zcvf versions/web_static_$(date +%Y%m%d%H%M%S).tgz\
+        web_static")
+        print("web_static packed: {} -> {}".
+              format(filepath, os.path.getsize(filepath)))
+    except:
+            return None
